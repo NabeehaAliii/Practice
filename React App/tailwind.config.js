d67ba37d72bd6a03f323d669200c00ce29718538
Nabeeha Ali <69257476+Nabeehaali2004@users.noreply.github.com>
2024-10-09 17:37:03 +0500

    Add Project

---
+++ React App/tailwind.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      screen:{
+        // 'md': '789px'
+
+      }
+    },
+  },
+  plugins: [],
+}
+
